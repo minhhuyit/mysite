550d1408d2922b4bdab74a912d9a202f16a168f9
minhhuyit <minhhuy.bkit308@gmail.com>
2016-06-20 23:43:38 +0700

    'test'

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basicfweifhwef',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
